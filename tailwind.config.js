39a6e42ea60b7308404aaff20589e38037ff4a5b
justinflangan123 <justin.flanagan@student.swic.edu>
2024-03-09 20:13:09 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html"],
-    theme: {
-      extend: {},
-    },
-    plugins: [require("@tailwindcss/forms")],
-  };
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
